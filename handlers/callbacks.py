a26aa1ea2d6b2355a801fe1ffaa80a8f1818d0a8
Develor <shimanokaii@gmail.com>
2023-11-30 22:49:11 +0200

    Added answers to callbacks

--- handlers/callbacks.py
+++ handlers/callbacks.py
@@ -16,11 +16,13 @@ bot = Bot(token=config.token.get_secret_value(), parse_mode='HTML')
 
 @router.callback_query(F.data=='tiktok')
 async def cb_tiktok(cb: CallbackQuery, state: FSMContext):
+    await cb.answer()
     await state.set_state(PickState.tt_assistant)
     await cb.message.answer(text.tt_assistant_menu, reply_markup=inline.tiktok_assistant_kb)
 
 @router.callback_query(F.data=='download_video')
 async def cb_download_video(cb: CallbackQuery, state: FSMContext):
+    await cb.answer()
     await state.set_state(PickState.tt_downloading)
     await cb.message.answer(text.tt_video_download)
 
@@ -51,11 +53,13 @@ async def wrong_format_alert(msg: Message):
 
 @router.callback_query(F.data=='generate_tags')
 async def cb_generate_tags(cb: CallbackQuery, state: FSMContext):
+    await cb.answer()
     await state.set_state(PickState.tt_generating_tags)
     await cb.message.answer(text.tt_assistant_menu, reply_markup=inline.tiktok_assistant_kb)
 
 @router.callback_query(F.data=='feedback')
 async def cb_send_feedback(cb: CallbackQuery, state: FSMContext):
+    await cb.answer()
     await state.set_state(PickState.sending_feedback)
     await cb.message.answer(text.send_feedback)
 
