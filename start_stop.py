4f2d4a0073718e040cb651414df8f40f69e62c62
Develor <shimanokaii@gmail.com>
2023-11-30 23:21:34 +0200

    Edited text and its output

--- start_stop.py
+++ start_stop.py
@@ -1,8 +1,10 @@
+import text
+
 from aiogram import Bot
 from config_reader import config
 
 async def bot_start(bot: Bot):
-    await bot.send_message(config.admin_id.get_secret_value(), text='✅ <b>Bot launched!</b>')
+    await bot.send_message(config.admin_id.get_secret_value(), text= text.bot_start)
 
 async def bot_stop(bot: Bot):
-    await bot.send_message(config.admin_id.get_secret_value(), text='❌ <b>Bot stopped!</b>')
+    await bot.send_message(config.admin_id.get_secret_value(), text= text.bot_stop)
